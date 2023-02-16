f32734e6b6636930bd966091bbcfe8d404b798f3
SimonMuthungu <henrymsechu1@gmail.com>
2023-02-16 06:11:31 +0300

    unittest for a simple area function

--- compute_area.py
+++ compute_area.py
@@ -1,14 +1,20 @@
 # This Python file uses the following encoding: utf-8
 
+
 from math import pi
 
 def circle_area(r):
+    if type(r) not in [int, float]: 
+        raise TypeError
+    if r < 0:
+        raise ValueError("negatives must not be used!")
     return pi*(r**2)
 
-# Test function
-radii = [2, 0, -3, 2 + 5j, True, "radius"]
-message = "Area of circles with r = {radius} is {area}"
+if __name__ == "main":
+    # Test function
+    radii = [2, 0, -3, 2 + 5j, True, "radius"]
+    message = "Area of circles with r = {radius} is {area}"
 
-for r in radii:
-    Area = circle_area(r)
-    print(message.format(radius=r, area=Area))
+    for r in radii:
+        Area = circle_area(r)
+        print(message.format(radius=r, area=Area))
--- test_area.py
+++ test_area.py
@@ -5,9 +5,16 @@ from math import pi
 class TestAreaFunction(unittest.TestCase):
     def test_area(self):
         # Test radius when radius >= 0
-        self.assertAlmostEquals(circle_area(1), pi)
-        self.assertAlmostEquals(circle_area(0), 0)
-        self.assertAlmostEquals(circle_area(2.1), pi*2.1**2)
+        self.assertAlmostEqual(circle_area(1), pi)
+        self.assertAlmostEqual(circle_area(0), 0)
+        self.assertAlmostEqual(circle_area(2.1), pi* 2.1**2)
 
-# if __name__ == '__main__':
-#     unittest.main()
+    def test_values(self):
+        # See if we get value error on negative numbers
+        self.assertRaises(ValueError, circle_area, -2)
+
+    def test_types(self):
+        # see whether it raises when given non real integers
+        self.assertRaises(TypeError, circle_area, "radius")
+        self.assertRaises(TypeError, circle_area, 2 + 5j)
+        self.assertRaises(TypeError, circle_area, True)
